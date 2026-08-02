094e21e79fe9d8f8f7984af127487825a7b4f4e5
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2026-08-02 23:06:17 +0900

    chore: disable require JSDoc

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "eslint/config";
-import { eslintNextConfig } from "@robot-inventor/eslint-config";
+import { eslintNextConfigNoJSDoc } from "@robot-inventor/eslint-config";
 
-export default defineConfig(eslintNextConfig, {
+export default defineConfig(eslintNextConfigNoJSDoc, {
     rules: {
         "sort-keys": "off"
     }
