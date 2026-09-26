6c0a4016e8f3c80ae39c3e91e3595140b31bfd9d
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2026-09-26 23:55:58 +0900

    feat: update icon for the [Login with a passkey] button (#175)

---
+++ .changeset/large-cows-admire.md
@@ -0,0 +1,5 @@
+---
+"payload-passkey": minor
+---
+
+feat: update icon for the [Login with a passkey] button
---
+++ src/components/PasskeyIcon.tsx
@@ -0,0 +1,24 @@
+import type { ReactNode } from "react";
+
+const PasskeyIcon = (): ReactNode => (
+    // https://github.com/lucide-icons/lucide/blob/de9b2d39b7ec080991717019acdc8b49caf91a22/icons/user-key.svg
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <path d="M20 11v6" />
+        <path d="M20 13h2" />
+        <path d="M3 21v-2a4 4 0 0 1 4-4h6a4 4 0 0 1 2.072.578" />
+        <circle cx="10" cy="7" r="4" />
+        <circle cx="20" cy="19" r="2" />
+    </svg>
+);
+
+export { PasskeyIcon };
